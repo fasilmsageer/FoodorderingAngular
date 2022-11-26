819699ce49ec049711fb6e84dd63a6c24e16b93a
fasilmsageer <fasilmsageer@gmail.com>
2022-11-26 19:25:37 +0530

    southind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { HomeComponent } from './home/home.component';
+import { SouthIndianComponent } from './south-indian/south-indian.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLoginComponent,
     UserRegistrationComponent,
-    HomeComponent
+    HomeComponent,
+    SouthIndianComponent
   ],
   imports: [
     BrowserModule,
